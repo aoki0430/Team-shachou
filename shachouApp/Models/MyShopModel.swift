1564cfdefd801ed27f67d3de4f79bd85679e09b6
aoki0430 <31432520+garuda02@users.noreply.github.com>
2018-03-14 03:17:17 +0900

    コンパイルエラーを倒した！

--- shachouApp/Models/MyShopModel.swift
+++ shachouApp/Models/MyShopModel.swift
@@ -1,20 +1,21 @@
 import Foundation
 import Alamofire
 
-final class MyShopModel {
-    var shop: Shop?
-    
-    func changeShopImage(imageURLString: String, completion: @escaping () -> Void) {
-        guard let email = email else { return }
-        let headers = [
-            "Uid": email,
-        ]
-        
-        let params: [String:Any] = [
-            "user": [
-                "image_url": imageURLString
-            ]
-        ]
-        
-    }
-}
+//final class MyShopModel {
+//    var shop: Shop?
+//
+//    func changeShopImage(imageURLString: String, completion: @escaping () -> Void) {
+//        guard let email = email else { return }
+//        let headers = [
+//            "Uid": email,
+//        ]
+//
+//        let params: [String:Any] = [
+//            "user": [
+//                "image_url": imageURLString
+//            ]
+//        ]
+//
+//    }
+//}
+
