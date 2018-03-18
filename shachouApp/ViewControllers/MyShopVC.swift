758f3a95289de9c610bebaa24ecd8e5b3fce7949
aoki0430 <31432520+garuda02@users.noreply.github.com>
2018-03-18 19:48:57 +0900

    カメラのplist設定

--- shachouApp/ViewControllers/MyShopVC.swift
+++ shachouApp/ViewControllers/MyShopVC.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import SnapKit
 
 class MyShopVC: UIViewController {
+    
+    let cameraBtn: UIButton = {
+        let btn = UIButton()
+        btn.backgroundColor = .black
+        return btn
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "お店1"
+        
+        self.view.addSubview(cameraBtn)
+        cameraBtn.addTarget(self, action: #selector(cameraBtnDidTap), for: .touchUpInside)
+        
+        cameraBtn.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        
+    }
+    
+    @objc func cameraBtnDidTap() {
+        
     }
     
     override func didReceiveMemoryWarning() {
