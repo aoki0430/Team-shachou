0af8de62963c3ce44d3b609cc168d1e85194879a
aoki0430 <31432520+garuda02@users.noreply.github.com>
2018-03-20 00:33:42 +0900

    imagepickerを実装しました

--- shachouApp/ViewControllers/MyShopVC.swift
+++ shachouApp/ViewControllers/MyShopVC.swift
@@ -8,30 +8,54 @@
 
 import UIKit
 import SnapKit
+import Photos
+import DKImagePickerController
 
 class MyShopVC: UIViewController {
     
     let cameraBtn: UIButton = {
         let btn = UIButton()
         btn.backgroundColor = .black
+        btn.setTitle("カメラまなだよ！", for: .normal)
         return btn
     }()
+    
+    let imageView = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "お店1"
         
         self.view.addSubview(cameraBtn)
+        self.view.addSubview(imageView)
+        
         cameraBtn.addTarget(self, action: #selector(cameraBtnDidTap), for: .touchUpInside)
         
         cameraBtn.snp.makeConstraints {
             $0.center.equalToSuperview()
         }
         
+        imageView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        
     }
     
     @objc func cameraBtnDidTap() {
+        let pickerController = DKImagePickerController()
         
+        pickerController.didSelectAssets = { (assets: [DKAsset]) in
+            print("didSelectAssets")
+            for asset in assets {
+                asset.fetchFullScreenImage(true, completeBlock: { (image, info) in
+                    if let image = image {
+                        self.imageView.image = image
+                    }
+                })
+            }
+        }
+        
+        self.present(pickerController, animated: true) {}
     }
     
     override func didReceiveMemoryWarning() {
